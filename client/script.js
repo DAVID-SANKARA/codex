772859ca259dd904ac35e1e2287c1abafec794f3
SANKARA <83004392+DAVID-SANKARA@users.noreply.github.com>
2023-05-06 13:40:20 +0000

    Update script.js

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ function typeText(element, text) {
   loader(messageDiv);
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codexx-uzpz.onrender.com', {
     method: 'POST',
     headers: {
       'content-type': 'application/json'
